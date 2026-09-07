7f7a59255ac24e6e9567f03aad77358edd1c67d6
Cifer4869 <2680154657@qq.com>
2026-09-07 18:20:36 +0800

    init: add lecture1~2 source and notes, ignore build outputs

---
+++ lecture_files/lecture_1~2/hello.h
@@ -0,0 +1,8 @@
+#ifndef HELLO_H_
+#define HELLO_H_
+
+// 函数声明：告诉编译器存在该函数，实现在 libhello.cpp 中
+void printHello();
+
+#endif
+
---
+++ lecture_files/lecture_1~2/libhello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hello.h"
+
+void printHello()
+{
+    std::cout << "Hello SLAM static library!" << '\n';
+}
+
